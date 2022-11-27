85d1a9557000b940df313d7e453e55960002a126
athai026 <athai026@ucr.edu>
2022-11-26 17:00:48 -0800

    init

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+void forward_selection();
+void backward_elimination();
+void LOO_cross_validation();
+
+int main () {
+
+
+
+
+    return 0;
+}
+
+void forward_selection() {
+
+}
+
+void backward_elimination() {
+
+}
+
+void LOO_cross_validation() {
+    
+}
